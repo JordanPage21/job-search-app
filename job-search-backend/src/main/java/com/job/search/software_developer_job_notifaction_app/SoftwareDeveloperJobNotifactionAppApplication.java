e967c416545d278bc2513f7a770bd6be875980e7
JordanPage21 <jpage58@gmail.com>
2025-02-21 11:03:06 -0500

    Init

---
+++ job-search-backend/src/main/java/com/job/search/software_developer_job_notifaction_app/SoftwareDeveloperJobNotifactionAppApplication.java
@@ -0,0 +1,13 @@
+package com.job.search.software_developer_job_notifaction_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDeveloperJobNotifactionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDeveloperJobNotifactionAppApplication.class, args);
+	}
+
+}
---
+++ job-search-backend/src/test/java/com/job/search/software_developer_job_notifaction_app/SoftwareDeveloperJobNotifactionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.search.software_developer_job_notifaction_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDeveloperJobNotifactionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
